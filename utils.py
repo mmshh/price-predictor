9d82f84578c7d9307b38f0518f69cbf8f0ec4fe7
Maysam Mokarian <mamokari@microsoft.com>
2019-12-15 14:28:32 -0500

    Adds utils

---
+++ utils.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def read_data_to_dataframe(path):
+    print(os.path.abspath(__file__))
+    return pd.read_csv(path)
+
+@staticmethod
+def split_data(df):
+    data = df.to_numpy()
+    X = data[:, :-1]
+    y = data[:, -1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    return X_train, X_test, y_train, y_test
